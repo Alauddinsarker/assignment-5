bd73e0d2d7bf8817e2fdac34a29e4c8f5ff6e3b6
Alauddinsarker <alauddinsarker@gmail.com>
2024-09-22 23:40:24 +0600

    Js one part done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Hi");
+document.getElementById("donate-now").addEventListener("click", function () {
+  const noakhaliAmount = parseFloat(
+    document.getElementById("noakhali-amount").value
+  );
+
+  const donationableAmount = parseFloat(
+    document.getElementById("donationable-amount").innerText
+  );
+
+  const donatedAmount = parseFloat(
+    document.getElementById("donated-amount").innerText
+  );
+  document.getElementById("donated-amount").innerText =
+    noakhaliAmount + donatedAmount;
+  document.getElementById("donationable-amount").innerText =
+    donationableAmount - noakhaliAmount;
+  document.getElementById("noakhali-amount").value = "";
+  //   console.log(donatedAmount);
+});
